1b10b4242d2daad0a69403d6968dfcb7e7f88f7e
Reiji <88034696+forsyth47@users.noreply.github.com>
2023-05-03 16:45:08 +0530

    bruh

---
+++ api/keys.py
@@ -0,0 +1,14 @@
+import os
+
+#Your telegram username here.
+admin_username = "joshuaforsyth"
+
+#token = "YOUR-TELEGRAM-BOT-APIKEY"
+token = '6117775438:AAGelTeJICoBc79PaL3ZZJ78c3wYZTsB81Q'
+
+
+#apiurl = "https://api.consumet.org"
+#apiurl = "https://api.animxeast.eu.org"
+#apiurl = "https://c.delusionz.xyz"
+#apiurl = "https://api.haikei.xyz"
+apiurl = "https://conmet.vercel.app"
